74414ab1da9c5b81e7b6d401c6b22d4ff231a11f
Japhet Palmares <japhpalmares@github.com>
2025-06-11 14:05:54 +0800

    MVP Wedding Update: Navbar Text-align

--- js/script.js
+++ js/script.js
@@ -32,11 +32,11 @@ $(function () {
 
 // NAVBAR BURGER MENU
 document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.getElementById('menu-toggle');
-  const nav = document.getElementById('nav-links');
+  const toggleBtn = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
 
-  toggle.addEventListener('click', function () {
-    nav.classList.toggle('active');
+  toggleBtn.addEventListener('click', function () {
+    navLinks.classList.toggle('active');
   });
 });
 
